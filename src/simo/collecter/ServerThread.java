1c01590d07823875b49fcf71c51c6e9a0a7f7459
U-Binghao-PC\Binghao <Binghao@BINGHAO-PC.adns.unsw.edu.au>
2014-03-05 16:25:17 +1100

    add something here

--- src/simo/collecter/ServerThread.java
+++ src/simo/collecter/ServerThread.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
